22564a6274436dd01d378087ece511526344b19c
Martin Dogandzhiyski <94994594+MartinDogandzhiyski@users.noreply.github.com>
2022-12-17 18:50:35 +0200

    update

--- cars_universe/accounts/views.py
+++ cars_universe/accounts/views.py
@@ -25,43 +25,43 @@ def page_not_found(request):
     return render(request, 'page_not_found.html')
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserModel
-        fields = ('username', 'password')
-
-    def create(self, validated_data):
-        user = super().create(validated_data)
-
-        user.set_password(user.set_password)
-        user.save()
-        return user
-
-    def validate(self, data):
-        # Invoke password validators
-        user = UserModel(**data)
-        password = data.get('password')
-        errors = {}
-        try:
-            validators.validate_password(password, user)
-        except exceptions.ValidationError as e:
-            errors['password'] = list(e.messages)
-        if errors:
-            raise serializers.ValidationError(errors)
-        return super().validate(data)
-
-    def to_representation(self, instance):
-        user_representation = super().to_representation(instance)
-        user_representation.pop('password')
-        return user_representation
-
-
-class RegisterApiView(rest_generic_views.CreateAPIView):
-    pass
-
-
-class LoginApiView(authtoken_views.ObtainAuthToken):
-    pass
+# class CreateUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserModel
+#         fields = ('username', 'password')
+#
+#     def create(self, validated_data):
+#         user = super().create(validated_data)
+#
+#         user.set_password(user.set_password)
+#         user.save()
+#         return user
+#
+#     def validate(self, data):
+#         # Invoke password validators
+#         user = UserModel(**data)
+#         password = data.get('password')
+#         errors = {}
+#         try:
+#             validators.validate_password(password, user)
+#         except exceptions.ValidationError as e:
+#             errors['password'] = list(e.messages)
+#         if errors:
+#             raise serializers.ValidationError(errors)
+#         return super().validate(data)
+#
+#     def to_representation(self, instance):
+#         user_representation = super().to_representation(instance)
+#         user_representation.pop('password')
+#         return user_representation
+#
+#
+# class RegisterApiView(rest_generic_views.CreateAPIView):
+#     pass
+#
+#
+# class LoginApiView(authtoken_views.ObtainAuthToken):
+#     pass
 
 
 # class LogoutApiView(rest_views.APIView):
